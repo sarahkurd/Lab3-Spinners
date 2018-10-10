ff573c4d6d0ead2c438df8f441f7392f243ec3c3
sarahkurd <skurdogh@calpoly.edu>
2018-10-10 16:38:22 -0700

    initial commit

---
+++ app/src/main/java/com/example/lab3_spinners/MainActivity.kt
@@ -0,0 +1,53 @@
+package com.example.lab3_spinners
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
+
+    val names = listOf<String>("Sarah", "Dan", "Christina", "Maya", "Kevin", "Chris", "Matt", "Emma")
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Spinner 2
+        val spinner2: Spinner = findViewById(R.id.spinner2)
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter.createFromResource(
+                this,
+                R.array.nba_array,
+                android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            // Specify the layout to use when the list of choices appears
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            // Apply the adapter to the spinner
+            spinner2.adapter = adapter
+        }
+
+        val spinner: Spinner = findViewById(R.id.spinner1)
+        spinner.onItemSelectedListener = this
+
+        // Spinner 3
+        spinner3.adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, names)
+
+    }
+
+    // Spinner 1
+    override fun onItemSelected(parent: AdapterView<*>, view: View, pos: Int, id: Long) {
+        // An item was selected. You can retrieve the selected item using
+        // parent.getItemAtPosition(pos)
+        val selection = parent.getItemAtPosition(pos) as? String
+        Toast.makeText(this, selection, Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onNothingSelected(parent: AdapterView<*>) {
+        // Another interface callback
+    }
+}
